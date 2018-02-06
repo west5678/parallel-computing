8571d55ea6237d4b9f5fa9f3d2b39f0890567679
Wei Li <west5678@c221-604.maverick.tacc.utexas.edu>
2018-02-05 21:00:10 -0600

    modified code according to instruction

--- hw0/hw0.cpp
+++ hw0/hw0.cpp
@@ -2,49 +2,51 @@
 #include <vector>
 #include <stdlib.h>
 
-#include<grvy.h>	//grvy for performance timing
+//#include<grvy.h>	//grvy for performance timing
 #include<sys/time.h>
 #include<time.h>
 #include<unistd.h>
 
 // Convenience timer macros for begin/end functions
 //
-#define FUNC_BEGIN_TIMER gt.BeginTimer(__func__);
-#define FUNC_END_TIMER   gt.EndTimer  (__func__);
+//#define FUNC_BEGIN_TIMER gt.BeginTimer(__func__);
+//#define FUNC_END_TIMER   gt.EndTimer  (__func__);
 
-GRVY::GRVY_Timer_Class gt;              // GRVY Timer
+//GRVY::GRVY_Timer_Class gt;              // GRVY Timer
 
-std::vector<std::vector<double>> randMatrix (int m, int n){
-	std::vector<std::vector<double>> x (m, std::vector<double> (n, 0.0));
-	FUNC_BEGIN_TIMER;
-	for (int i = 0; i < m; i++){
+typedef std::vector<std::vector<float>> Array2D;
+
+void initialize (Array2D& x, int n){
+	//FUNC_BEGIN_TIMER;
+	for (int i = 0; i < n; i++){
+		std::vector<float> row;
 		for (int j = 0; j < n; j++){
-			x[i][j] = ((double) rand() / (RAND_MAX));
+			row.push_back( (float)rand() / (RAND_MAX) );
 		}
+		x.push_back(row);
 	}
-	FUNC_END_TIMER;
-	return x;
+	//FUNC_END_TIMER;
 }
 
-std::vector<std::vector<double>> smooth (int m, int n, double a, double b, 
-	double c, std::vector<std::vector<double>> x){
-	FUNC_BEGIN_TIMER;
-	std::vector<std::vector<double>> y (m, std::vector<double> (n, 1.0));
-	for (int i=1; i<m-1; i++){
+void smooth (const Array2D x, Array2D& y, int n, float a, float b, 
+	float c){
+	//FUNC_BEGIN_TIMER;
+	for (int i = 0; i < n; i++){
+		y.push_back(std::vector<float> (n, 0.0));
+	} 
+	for (int i=1; i<n-1; i++){
 		for (int j=1; j<n-1; j++){
 			y[i][j] = a * (x[i-1][j-1] + x[i-1][j+1] + x[i+1][j-1]
 			+ x[i+1][j+1]) + b * (x[i-1][j] + x[i+1][j] 
 			+ x[i][j-1] + x[i][j+1]) + c * x[i][j];
 		}
 	}
-	std::cout.precision(15);
-	FUNC_END_TIMER;
-	return y;
+	//FUNC_END_TIMER;
 }
 
-int count(std::vector<std::vector<double>> x, double t){
+int count(const std::vector<std::vector<float>> x, float t){
 	int res = 0;
-	FUNC_BEGIN_TIMER;
+	//FUNC_BEGIN_TIMER;
 	for (auto row: x){
 		for (auto c : row){
 			if (c < t){
@@ -52,37 +54,49 @@ int count(std::vector<std::vector<double>> x, double t){
 			}
 		}
 	}
-	FUNC_END_TIMER;
+	//FUNC_END_TIMER;
 	return res;
 }
 
 int main(){
-	gt.Init("hw0 Timing");
-	gt.BeginTimer("Main Program");
+	//gt.Init("hw0 Timing");
+	//gt.BeginTimer("Main Program");
 
-	//generate first m*n array(x) with random numbers
-	int m, n;
-	double a, b, c, t;
-	m = 10;
-	n = 10;
-	a = 0.0;
-	b = 0.125;
-	c = 0.5;
-	t = 0.5;
+	//size of matrix (nxn)
+	int n = 16384+2;
+
+	//convolution constants
+	float a, b, c, t;
+	a = 0.05;
+	b = 0.1;
+	c = 0.4;
+
+	//threshold t
+	t = 0.1;
+
 	//generate random matrix
-	std::vector<std::vector<double>> x = randMatrix(m, n);
+	Array2D x;
+	initialize(x, n);	
 	
 	//smooth matrix x
-	std::vector<std::vector<double>> y = smooth(m, n, a, b, c, x);
+	Array2D y;
+	smooth(x, y, n, a, b, c);
 
 	//Counting of elements below threshold 
+	std::cout << "total number of elements in a column/row of an array: " <<n << std::endl;
+	std::cout << "total number of inner elements in a column/row of an array: " << n - 2 << std::endl;
+	std::cout << "total number of elements in an array: " << n*n << std::endl;
+	std::cout << "total number of inner elements in an array: " << (n-2)*(n-2)<<std::endl;
+	std::cout << "Size of array: " << sizeof(x) << std::endl;
 	std::cout << "# of elements in x below threshold: " << count(x, t) << std::endl;
 	std::cout << "# of elements in y below threshold: " << count(y, t) << std::endl;
+	std::cout << x.size()*x[0].size()*sizeof(x[0][0]) << std::endl;
 
+/*
 	gt.EndTimer("Main Program");
 	gt.Finalize();
 	gt.Summarize();
 	gt.Reset();
-
+*/
 	return 0;
 }
